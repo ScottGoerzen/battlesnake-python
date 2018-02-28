01fa1955dbc865ada4c4db955a102a45caf6aa8e
Scott Goerzen <sgoerzen@uvic.ca>
2018-02-28 13:05:51 -0800

    Test

--- app/main.py
+++ app/main.py
@@ -83,7 +83,7 @@ def closest(items, start):
 def init(data):
     grid = [[0 for col in xrange(data['height'])] for row in xrange(data['width'])]
     for snek in data['snakes']:
-        print(snek)
+        print(snek[0])
         print ('hi')
         if snek['data']['id'] == ID:
             mysnake = snek
