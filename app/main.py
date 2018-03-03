c0a0bd9ac27bdd51f8ed9514668d907f98a63c99
Scott Goerzen <sgoerzen@uvic.ca>
2018-03-03 10:53:13 -0800

    Stuff

--- app/main.py
+++ app/main.py
@@ -234,16 +234,16 @@ def move():
     if snek['health'] < 75:
         if head[1]+1 < height:
             dist1 = distance([head[0], head[1]+1], [closeFood['x'], closeFood['y']])
-            grid[head[0]][head[1]+1] += dist1+healthF
+            grid[head[0]][head[1]+1] += dist1-healthF
         if head[1]-1 >= 0:
             dist2 = distance([head[0], head[1]-1], [closeFood['x'], closeFood['y']])
-            grid[head[0]][head[1]-1] += dist2+healthF
+            grid[head[0]][head[1]-1] += dist2-healthF
         if head[0]+1 < width:
             dist3 = distance([head[0]+1, head[1]], [closeFood['x'], closeFood['y']])
-            grid[head[0]+1][head[1]] += dist3+healthF
+            grid[head[0]+1][head[1]] += dist3-healthF
         if head[0]-1 >= 0:
             dist4 = distance([head[0]-1, head[1]], [closeFood['x'], closeFood['y']])
-            grid[head[0]-1][head[1]] += dist4+healthF
+            grid[head[0]-1][head[1]] += dist4-healthF
 
     #chase tail
     if snek['health'] > 75:
