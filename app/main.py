4584fcd918c86012e419e9d2241dffa58e9c74ce
Scott Goerzen <sgoerzen@uvic.ca>
2018-03-03 11:28:42 -0800

    Stuff

--- app/main.py
+++ app/main.py
@@ -123,14 +123,14 @@ def init(data):
                 grid[coord['x'] + 1][coord['y']] += ENESNAKE
             if coord['x'] - 1 < 0:
                 grid[coord['x'] - 1][coord['y']] += ENESNAKE
-            if coord['y'] + 1 < data['height'] and coord['x'] + 1 < data['width']:
+            '''if coord['y'] + 1 < data['height'] and coord['x'] + 1 < data['width']:
                 grid[coord['x'] + 1][coord['y'] + 1] += ENESNAKE / 2
             if coord['y'] + 1 < data['height'] and coord['x'] - 1 >= 0:
                 grid[coord['x'] - 1][coord['y'] + 1] += ENESNAKE / 2
             if coord['y'] - 1 >= 0 and coord['x'] + 1 < data['width']:
                 grid[coord['x'] + 1][coord['y'] - 1] += ENESNAKE / 2
             if coord['y'] - 1 >= 0 and coord['x'] - 1 >= 0:
-                grid[coord['x'] - 1][coord['y'] - 1] += ENESNAKE / 2
+                grid[coord['x'] - 1][coord['y'] - 1] += ENESNAKE / 2'''
 
 
     for f in data['food']['data']:
